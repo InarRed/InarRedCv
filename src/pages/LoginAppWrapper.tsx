b20843bbd9ec110d301f1a85f3716f509fe82450
Inar <Inar2000s@mail.ru>
2022-11-02 19:30:04 +0300

    useEffect

--- src/pages/LoginAppWrapper.tsx
+++ src/pages/LoginAppWrapper.tsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AppContext } from '../data/AppContext';
 import BasicLayout from './BasicLayout';
-import { useMount } from '../utils/useMount';
 
 const LoginAppWrapper = () => {
   const { userStore } = useContext(AppContext);
-  useMount(() => {
-    userStore.loadProfile();
+  useEffect(() => {
+    console.log('Start loading profile');
+    const result = userStore.loadProfile();
+    result.then(() => console.log('end loading'));
+    result.catch((e) => console.log(`Error loading profile${e}`));
   });
   // useEffect(() => {
   //   userStore.getProfile();
